6b8e8ccc0513e274cfd87d56689266f4651f0b2b
MarianaKely <marianakely.r@gmail.com>
2023-04-28 22:30:12 -0300

    deleting 'Link' from Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { BsHandbag } from 'react-icons/bs';
 import { FaGamepad } from 'react-icons/fa'
